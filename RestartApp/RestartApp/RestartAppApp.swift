957b714fa4b81cd646fd8c7585f77be096e44fc8
Ramiro Aguirre <ram@192.168.1.11>
2023-09-08 16:20:33 -0600

    Started XCode project

---
+++ RestartApp/RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Ramiro Aguirre on 08/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
